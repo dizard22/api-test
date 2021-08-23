e91da492c045df97430ad60bf940f0719c88cbda
Nel Pla <nel.pla@col.vueling.com>
2021-08-23 20:31:35 +0200

    test

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+application = Flask(__name__)
+app = application
+
+
+@app.route('/')
+def index():
+    return 'test-api'
+
+
+@app.route('/drinks')
+def get_drinks():
+    return {"drinks": "drink data"}
+
